ccb6288c01205faa89f8d7ef29808adfe4ff1824
YendetiYashashwini <yendetiyashashwini02@gmail.com>
2026-07-05 12:50:45 +0530

    Arc 2 - updated file path

--- arc2/arc2.py
+++ arc2/arc2.py
@@ -6,7 +6,7 @@ import os
 from dotenv import load_dotenv
 
 # Load API key from .env file
-load_dotenv(r"c:\Users\yende\Projects\Food Agent\.env")
+load_dotenv(r"c:\Users\yende\Projects\AI Food Ordering Agent\.env")
 
 # Connect to Groq
 llm = OpenAI(
